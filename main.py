f4375ccddba6cffe94bf10747ec18844a34daebf
bilalmaq01 <bilalmaq01@gmail.com>
2025-03-21 14:01:52 -0500

    Added the player, make him rotate and move, added asteroids and so far when the player collides with asteroids game ends, player can shoot

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
+
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    launched = True
-    while launched:
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers =(asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+    Shot.containers = (shots,updatable,drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+
+        updatable.update(dt)
+        for ass in asteroids:
+            uhohcheck = ass.collision(player)
+            if uhohcheck == True:
+                print("Game Over!")
+                sys.exit()
+        
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-    
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
